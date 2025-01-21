6cb935ac2eac6ecc6cda095ce324e20929f678b6
m-mueller678 <marcus.mueller.678@gmail.com>
2025-01-21 22:08:11 +0100

    add get test

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,5 @@
 use borrow_channel::BorrowChannel;
+use std::cell::RefCell;
 use std::rc::Rc;
 
 const _: () = assert!(cfg!(feature = "unsafe_disable_abort"));
@@ -18,7 +19,7 @@ fn make_string(n: usize) -> String {
 }
 
 #[test]
-fn local() {
+fn get_mut() {
     let channel = Rc::new(BorrowChannel::<&String, _>::new_unsync());
     let mut_channel = Rc::new(BorrowChannel::<&mut String, _>::new_unsync());
     let reads_a = || {
@@ -28,4 +29,23 @@ fn local() {
     let mut a = make_string(0);
     mut_channel.lend(&mut a, writes_a);
     channel.lend(&mut a, reads_a);
+}
+
+#[test]
+fn ref_cell() {
+    let channel = Rc::new(BorrowChannel::<&RefCell<String>, _>::new_unsync());
+    let reads_a = || {
+        assert_eq!(make_string(1), *channel.borrow().get().borrow());
+    };
+    let writes_a = || *channel.borrow().get().borrow_mut() = make_string(1);
+    let multi_guard = || {
+        let g1 = channel.borrow();
+        let mut g2 = channel.borrow();
+        *g1.get().borrow_mut() = make_string(2);
+        assert_eq!(*g2.get_mut().borrow(), make_string(2));
+    };
+    let a = RefCell::new(make_string(0));
+    channel.lend(&a, writes_a);
+    channel.lend(&a, reads_a);
+    channel.lend(&a, multi_guard);
 }
