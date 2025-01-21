9cdca21ebfc42f1b2ed794b159d85b2c02ef42ab
m-mueller678 <marcus.mueller.678@gmail.com>
2025-01-21 14:45:57 +0100

    add docs, public traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
 #![no_std]
+//! A channel for sending borrows.
+//! This is useful when you want to borrow data but cannot accept a reference to it via function arguments.
+//! For example, because a closure is called by code you do not control and which is unaware of the borrowing.
+//!
+//! A reference can be inserted via [lend](BorrowChannel::lend).
+//! While the function passed to lend executes, the reference can be reborrowed using [borrow](BorrowChannel::borrow).
+//! If lend attempts to return while the data is still borrowed, the program is aborted.
 //!
 //! ```rust
 //! # use crate::borrow_channel::BorrowChannel;
@@ -26,6 +33,17 @@ use radium::marker::{Nuclear, NumericOps};
 use radium::{Isotope, Radium};
 use scopeguard::defer;
 
+/// This trait can be implemented on reference-like types to make them usable with BorrowChannel.
+///
+/// # Safety
+/// `Borrowed` must behave like a reference with regard to reborrowing:
+/// - Creating a `Borrowed<'a>` from a `Borrowed<'b>` by transmuting/copying its bytes must be safe if `'a` outlives `'b`.
+/// - Multiple such reborrowed copies may be made.
+/// - If `Shared` is `false`, users of the trait must not create copies with overlapping lifetimes.
+///
+/// Moreover, `Borrowed` must have no interior mutability (the unstable [Freeze](core::marker::Freeze) trait).
+/// This requirement is to enable implementing Deref.
+/// It may be relaxed once Freeze is stabilized.
 pub unsafe trait ChannelBorrow {
     const IS_SHARED: bool;
     type Borrowed<'a>;
@@ -41,12 +59,14 @@ pub struct SyncLock<T: CounterInner> {
 macro_rules! impl_channel_lock {
     ($T:ty) => {
         impl CounterInner for $T {}
-        impl ChannelLock for UnsyncLock<$T> {
+        impl CounterInnerPriv for $T {}
+        impl ChannelLock for UnsyncLock<$T> {}
+        impl ChannelLockPriv for UnsyncLock<$T> {
             type CounterInner = $T;
             type Counter = Cell<$T>;
         }
-
-        impl ChannelLock for SyncLock<$T> {
+        impl ChannelLock for SyncLock<$T> {}
+        impl ChannelLockPriv for SyncLock<$T> {
             type CounterInner = $T;
             type Counter = Isotope<$T>;
         }
@@ -58,30 +78,38 @@ impl_channel_lock!(u32);
 impl_channel_lock!(u16);
 impl_channel_lock!(u8);
 
-trait ChannelLock {
-    type CounterInner: CounterInner;
+#[allow(private_bounds)]
+pub trait ChannelLock: ChannelLockPriv {}
+
+trait ChannelLockPriv {
+    type CounterInner: CounterInnerPriv;
     type Counter: Radium<Item = Self::CounterInner>;
 }
 
-trait CounterInner: From<u8> + Shl<u32, Output = Self> + NumericOps + Debug + Copy + Nuclear {}
+#[allow(private_bounds)]
+pub trait CounterInner: CounterInnerPriv {}
+trait CounterInnerPriv:
+    From<u8> + Shl<u32, Output = Self> + NumericOps + Debug + Copy + Nuclear
+{
+}
 
-fn counter_bits<C: CounterInner>() -> u32 {
+fn counter_bits<C: CounterInnerPriv>() -> u32 {
     size_of::<C>() as u32 * 8
 }
 
-fn state_empty<C: CounterInner>() -> C {
+fn state_empty<C: CounterInnerPriv>() -> C {
     C::from(0u8)
 }
 
-fn state_locked<C: CounterInner>() -> C {
+fn state_locked<C: CounterInnerPriv>() -> C {
     C::from(1u8) << (counter_bits::<C>() - 2)
 }
 
-fn state_filled<C: CounterInner>() -> C {
+fn state_filled<C: CounterInnerPriv>() -> C {
     C::from(2u8) << (counter_bits::<C>() - 2)
 }
 
-fn state_mask<C: CounterInner>() -> C {
+fn state_mask<C: CounterInnerPriv>() -> C {
     C::from(3u8) << (counter_bits::<C>() - 2)
 }
 
@@ -106,7 +134,9 @@ impl<'a, T: ChannelBorrow, L: ChannelLock> Deref for BorrowChannelGuard<'a, T, L
 
     fn deref(&self) -> &Self::Target {
         unsafe {
-            transmute::<&MaybeUninit<T::Borrowed<'_>>, &T::Borrowed<'_>>(&*self.channel.data.get())
+            transmute::<&MaybeUninit<T::Borrowed<'static>>, &T::Borrowed<'_>>(
+                &*self.channel.data.get(),
+            )
         }
     }
 }
@@ -114,9 +144,8 @@ impl<'a, T: ChannelBorrow, L: ChannelLock> Deref for BorrowChannelGuard<'a, T, L
 impl<T: ChannelBorrow, L: ChannelLock> BorrowChannelGuard<'_, T, L> {
     pub fn with<'b>(&'b mut self, f: impl FnOnce(T::Borrowed<'b>)) {
         f(unsafe {
-            transmute::<T::Borrowed<'_>, T::Borrowed<'_>>(
-                (*self.channel.data.get()).assume_init_read(),
-            )
+            let ptr: *const MaybeUninit<T::Borrowed<'static>> = self.channel.data.get();
+            ptr.cast::<T::Borrowed<'_>>().read()
         })
     }
 }
@@ -134,6 +163,11 @@ impl<T: ChannelBorrow> BorrowChannel<T, SyncLock<u64>> {
 }
 
 impl<T: ChannelBorrow, L: ChannelLock> BorrowChannel<T, L> {
+    /// Create a BorrowChannel with an arbitrary `ChannelLock`.
+    /// Callers must ensure the lock will not overflow.
+    /// Consider using [new_sync](Self::new_sync) and [new_unsync](Self::new_unsync), which use a 62 bit counter, making overflow practically impossible.
+    ///
+    /// TODO: document when overflows might occur
     pub unsafe fn new() -> Self {
         BorrowChannel {
             data: UnsafeCell::new(MaybeUninit::uninit()),
